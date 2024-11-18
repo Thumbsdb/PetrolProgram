fcd93cf5c9fee64ab8b8c6911cf3b6c52d266a13
Chris Hadden <chrish2255@gmail.com>
2024-11-18 14:31:58 +0000

    FuelChange WIP

--- PetrolProgram/FuelChange.Designer.vb
+++ PetrolProgram/FuelChange.Designer.vb
@@ -22,9 +22,133 @@ Partial Class FuelChange
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "FuelChange"
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(40, 24)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(63, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Petrol cost"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(146, 24)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(65, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Diesel Cost"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(40, 47)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(146, 47)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(40, 76)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(40, 117)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 5
+        Label3.Text = "Label3"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(40, 102)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(203, 15)
+        Label4.TabIndex = 6
+        Label4.Text = "The Current Price of Petrol and Diesel"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(40, 132)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(41, 15)
+        Label5.TabIndex = 7
+        Label5.Text = "Label5"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(132, 132)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(41, 15)
+        Label6.TabIndex = 8
+        Label6.Text = "Label6"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(132, 117)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(41, 15)
+        Label7.TabIndex = 9
+        Label7.Text = "Label7"
+        ' 
+        ' FuelChange
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(367, 306)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "FuelChange"
+        Text = "FuelChange"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
 End Class
